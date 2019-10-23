7641a5de57eb13bb9076ebdca336d8dd530e737e
CIP-RIU <cip-riu@cgiar.org>
2019-10-23 17:43:30 +0000

    change directory for saving downloads

--- inst/hidap_agrofims/app.R
+++ inst/hidap_agrofims/app.R
@@ -143,18 +143,8 @@ ui <- dashboardPage(
     ),
     
     tags$head(
-  
-        "<script async src=\"https://www.googletagmanager.com/gtag/js?id=UA-150744979-1\"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', 'UA-150744979-1');
-          ga('send', 'pageview');
-        </script>
-        "
-      
+     
+     includeHTML(("www/analytics/google-analytics.html"))
     ),
     
     useShinyjs(),
